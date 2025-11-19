95c6d91cecb68f15a07f8ddbbd42b648c4701625
YuriBragaJR <yuri.bjr@gmail.com>
2025-11-19 17:19:45 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ export default function Hero() {
             <div className="row align-items-center">
                 <div className="col-lg-8 col-xl-8 col-xxl-6 text-lg-start text-center">
                     <h1 className="display-4 fw-bold mb-5 text-white txt-sh hero-title"><strong className='cor-p proposito'>Reescreva</strong> sua Trajetória com <strong className='cor-s proposito'>Propósito</strong></h1>
-                    <p className="lead mb-4 text-white txt-sh fw-medium hero-txt">Descubra caminhos para transformar sua trajetória profissional com orientação especializada e uma comunidade que apoia seu crescimento.</p>
-                    <a href="#contato" className="btn btn-warning bg-s btn-lg fw-medium px-4 me-2 hero-cta">Comece agora</a>
+                    <p className="lead mb-4 text-white txt-sh fw-medium hero-txt">Descubra caminhos para retransformar sua trajetória profissional com orientação especializada e uma comunidade que apoia seu crescimento.</p>
+                    <a href="#contato" className="btn btn-warning bg-s btn-lg fw-medium px-4 me-2 hero-cta">Recomece agora</a>
                 </div>
 
             </div>
